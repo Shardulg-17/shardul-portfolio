1ffd3278ef6ba3fd4b5c47d64deae15ec6431388
Shardul Gajbhiye <gajbhiyeshardul340@gmail.com>
2026-03-15 21:35:22 +0000

    Updates

--- assets/Navbar-BbYQPqeR.js
+++ assets/Navbar-BbYQPqeR.js
@@ -1,4 +1,4 @@
-import{j as S,r as Be}from"./index-BycTw7Kh.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j as S,r as Be}from"./index-DLULPwKz.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.12.7
  * https://gsap.com
  * 
